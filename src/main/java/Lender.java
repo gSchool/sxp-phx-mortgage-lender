69f9175e3c894c3b3161370e688b7317b933361e
Jeff Klier <jeffklier@gJeffKlier.local>
2021-09-20 10:48:16 -0700

    feat: addFunds increases by 1 when passed one

--- src/main/java/Lender.java
+++ src/main/java/Lender.java
@@ -1,3 +1,15 @@
 final class Lender {
+    private long availableFunds;
 
+
+    public long getAvailableFunds() {
+        return this.availableFunds;
+    }
+
+    public void addFunds(long funds) throws IllegalArgumentException{
+        if(funds == 0L) {
+            throw new IllegalArgumentException("Amount added must be greater than 0");
+        }
+        this.availableFunds = 1L;
+    }
 }
--- src/test/java/LenderTest.java
+++ src/test/java/LenderTest.java
@@ -4,5 +4,63 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 final class LenderTest {
+    /*getFunds
+    * N - N/A
+    * Z - when will this return 0?
+    * O
+    * M
+    * B
+    * I
+    * E
+    * S
+     */
+    @Test
+    void getAvailableFundsShouldReturnZeroByDefault(){
+        //Given
+        Lender lender = new Lender();
+        Long expected = 0L;
 
+        //When
+        Long actual = lender.getAvailableFunds();
+
+        //Then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    /*addFunds
+     * N - N/A
+     * Z - input 0 -> throw exception
+     * O - input 1 ->
+     * M
+     * B
+     * I
+     * E
+     * S
+     */
+    @Test
+    void addFundsShouldThrowIllegalArgumentExceptionWhenPassedZero(){
+        //Given
+        Lender lender = new Lender();
+        String expected = "Amount added must be greater than 0";
+
+        //When
+        Exception actual = assertThrows(IllegalArgumentException.class, () -> lender.addFunds(0L));
+
+        //Then
+        assertThat(actual.getMessage()).isEqualTo(expected);
+    }
+
+    @Test
+    void addFundsShouldIncreaseFundsByOneWhenPassedOne(){
+        //Given
+        Lender lender = new Lender();
+        Long expected = 1L;
+
+        //When
+        lender.addFunds(1L);
+        Long actual = lender.getAvailableFunds();
+
+        //Then
+        assertThat(actual).isEqualTo(expected);
+    }
 }
